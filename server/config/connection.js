5f86692ef14379aa382da2e20ecd4ce3beb9de76
mhamze23 <hamze.mohamed@live.ca>
2023-05-31 18:00:31 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-search-engine');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Book-engine-search');
 
 module.exports = mongoose.connection;
